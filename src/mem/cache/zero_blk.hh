26e0ac6ebe1f39590028cae373285829ca8ef1dd
Robert Kovacsics <rmk35@cl.cam.ac.uk>
2018-08-28 11:33:25 +0100

    TODO: Added zero_blk
    Change-Id: I3a69e71af9b58dccdcadea45c4aafac90b0366fd

---
+++ src/mem/cache/zero_blk.hh
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2018 Robert Kovacsics
+ * All rights reserved
+ *
+ * The license below extends only to copyright in the software and shall
+ * not be construed as granting a license to any other intellectual
+ * property including but not limited to intellectual property relating
+ * to a hardware implementation of the functionality of the software
+ * licensed hereunder.  You may use the software subject to the license
+ * terms below provided that you ensure that this notice is replicated
+ * unmodified and in its entirety in all distributions of the software,
+ * modified or unmodified, in source code or in binary form.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Robert Kovacsics
+ */
+
+#include <vector>
+
+#include "base/types.hh"
+#include "mem/cache/blk.hh"
+#include "mem/cache/tags/cacheset.hh"
+#include "mem/cache/tags/zero_tags.hh"
+
+class ZeroEntry
+{
+  public:
+    bool is_zero = false;
+    bool is_valid = false;
+    short way = 0;
+};
+
+class ZeroBlk : public CacheBlk
+{
+    // Needs a better name: in the simplest zero-cache, one 'cache
+    // line' or 'cache block' a bit (here one entry) indicates that
+    // the address that bit represents is all zero.
+    std::vector<ZeroEntry> entries;
+
+    ZeroTags *_owner;
+
+  public:
+    ZeroBlk();
+    virtual ~ZeroBlk();
+
+    void setOwner(BaseTags *owner) override;
+    ZeroTags *getOwner() const override;
+
+    virtual void setEntries(unsigned size);
+    virtual void setEntryValid(Addr addr, bool valid);
+    virtual void setEntryZero(Addr addr, bool zero);
+    virtual void setEntryWay(Addr addr, unsigned way);
+
+    virtual bool isEntryValid(Addr addr);
+    virtual bool isEntryZero(Addr addr);
+    virtual unsigned getEntryWay(Addr addr);
+
+    virtual std::vector<CacheBlk*>
+    getValidNonzeros(std::vector<CacheSet<CacheBlk>> sets);
+};
